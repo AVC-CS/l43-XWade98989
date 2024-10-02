1ad52623c4645e857406574f2b6282f26be1d0b7
XWade98989 <157772773+XWade98989@users.noreply.github.com>
2024-10-02 22:00:16 +0000

    done

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ def main():
     Code Your Program here
     ########################################
     """
+    i = 0
     total = 0
-
+    while i < 5:
+        i += 1
+        num = int(input())
+        total += num
     ########################################
     # Do not delete the return statement
     ########################################
